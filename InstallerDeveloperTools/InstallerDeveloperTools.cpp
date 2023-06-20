7afd4801123968198ff74814800f3cd545d21c5a
blackflame576 <pws15548@gmail.com>
2023-06-20 12:56:29 +0300

    Deploy

--- InstallerDeveloperTools/InstallerDeveloperTools.cpp
+++ InstallerDeveloperTools/InstallerDeveloperTools.cpp
@@ -37,12 +37,96 @@ string GetNameDistribution() {
     } 
 }
 
+class DownloadProgress : public IBindStatusCallback {
+    public:
+        HRESULT __stdcall QueryInterface(const IID &,void **) { 
+            return E_NOINTERFACE;
+        }
+        ULONG STDMETHODCALLTYPE AddRef(void) { 
+            return 1;
+        }
+        ULONG STDMETHODCALLTYPE Release(void) {
+            return 1;
+        }
+        HRESULT STDMETHODCALLTYPE OnStartBinding(DWORD dwReserved, IBinding *pib) {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE GetPriority(LONG *pnPriority) {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnLowResource(DWORD reserved) {
+            return S_OK;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnStopBinding(HRESULT hresult, LPCWSTR szError) {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE GetBindInfo(DWORD *grfBINDF, BINDINFO *pbindinfo) {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnDataAvailable(DWORD grfBSCF, DWORD dwSize, FORMATETC *pformatetc, STGMEDIUM *pstgmed) {
+            return E_NOTIMPL;
+        }        
+        virtual HRESULT STDMETHODCALLTYPE OnObjectAvailable(REFIID riid, IUnknown *punk) {
+            return E_NOTIMPL;
+        }
+
+        virtual HRESULT __stdcall OnProgress(ULONG ulProgress, ULONG ulProgressMax, ULONG ulStatusCode, LPCWSTR szStatusText)
+        {
+            
+            int percentage = static_cast<float>(ulProgress) / static_cast<float>(ulProgressMax) * 100;
+            // cout << percentage << endl;
+            if (TempPercentage != percentage && TempPercentage <= 98) {
+                // bar.update();
+                cout << percentage << endl;
+                TempPercentage = percentage;
+            }
+            return S_OK;
+        }
+};
+
 class Installer {
     public:
         void InstallDeveloperTools() {
-            
+            string dwnld_URL = "https://github.com/JetBrains/kotlin/releases/download/v1.8.22/kotlin-compiler-1.8.22.zip";
+            string savepath = "kotlin.zip";
+            if (OS_NAME == "Windows") {
+                DownloadProgress progress;
+                HRESULT download = URLDownloadToFile(NULL, dwnld_URL.c_str(), savepath.c_str(), 0, static_cast<IBindStatusCallback*>(&progress));
+                cout << "" << endl;
+            }
+            else if (OS_NAME == "macOS") {
+
+            }
+            else if (OS_NAME == "Linux") {
+
+            }
         }
     private:
+        // int Download(string url, string dir)
+        // {
+        //     try {
+        //         string name = (url.substr(url.find_last_of("/")));
+        //         string filename = dir + "/" + name.replace(name.find("/"), 1, "");
+        //         CURL* curl = curl_easy_init();
+        //         FILE* file = fopen(filename.c_str(), "wb");
+        //         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        //         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, FALSE);
+        //         curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, progress_func);
+        //         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        //         curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+        //         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        //         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteData);
+        //         curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+        //         CURLcode response = curl_easy_perform(curl);
+        //         curl_easy_cleanup(curl);
+        //         fclose(file);
+        //         cout << "" << endl;
+        //         return 200;
+        //     }
+        //     catch (string error) {
+        //         return 502;
+        //     }
+        // }
     // Installer();
     // ~Installer();
 };
--- InstallerDeveloperTools/InstallerDeveloperTools.hpp
+++ InstallerDeveloperTools/InstallerDeveloperTools.hpp
@@ -4,5 +4,9 @@
 using namespace  std;
 
 namespace InstallerDeveloperTools {
+    string OS_NAME;
+    int Percentage;
+    int TempPercentage = 0;
+    string NameDistribution;
     string ProjectDir = std::filesystem::current_path().generic_string();
 }
