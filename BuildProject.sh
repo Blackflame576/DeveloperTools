c722b01800e92ec22609af67f96e331784aee78f
blackflame576 <pws15548@gmail.com>
2023-08-11 17:09:25 +0300

    Rewrite BuildProject.sh

--- BuildProject.sh
+++ BuildProject.sh
@@ -5,38 +5,28 @@ case "${unameOut}" in
 	Darwin*) 	os="macOS";;
 	Linux*)		os="Linux";;
 esac
-if [ $os == "Linux" ]
-then
-  # Building
-	mkdir build
-  cd build
-  mkdir Linux
-  cd ..
-  g++ -o ./build/Linux/DeepForgeToolset ./src/DeepForgeToolset.cpp -DCURL_STATICLIB -I ../../include -I ./src/include -L ../../lib/  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
-	echo "==> Copying folder of DB to build/Linux ..."
-  cp --recursive ./src/DB ./build/Linux/DB
-  echo "==> Copying folder of DB to build/Windows was successfully."
-  echo "==> Copying folder of include to build/Linux"
-  cp --recursive ./src/include ./build/Linux/include
-  echo "==> Copying folder of include to build/Linux was successfully."
-  echo "==> Copying folder of Scripts to build/Linux"
-  cp --recursive ./src/Scripts ./build/Linux/Scripts
-  echo "==> Copying folder of Scripts to build/Linux was successfully."
-  echo "==> Copying folder of utils to build/Linux"
-  cp --recursive ./src/utils ./build/Linux/utils
-  echo "==> Copying folder of utils to build/Linux was successfully."
-  echo "==> Copying folder of locale to build/Linux"
-  cp --recursive ./src/locale ./build/Linux/locale
-  echo "==> Copying folder of locale to build/Linux was successfully."
-  echo "==> Copying folder of Logo to build/Linux"
-  cp --recursive ./src/Logo ./build/Linux/Icon
-  echo "==> Copying folder of Logo to build/Linux was successfully."
-elif [ $os == "macOS" ]
-then
-	cd build
-	cd MacOS
-	./DeepForgeToolset
-fi
+# Building
+mkdir build
+cd build
+mkdir Linux
+cd ..
+g++ -o ./build/$os/DeepForgeToolset ./src/DeepForgeToolset.cpp -DCURL_STATICLIB -I ../../include -I ./src/include -L ../../lib/  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
+echo "==> Copying folder of DB to build/$os ..."
+cp --recursive ./src/DB ./build/$os/DB
+echo "==> Copying folder of DB to build/$os was successfully."
+echo "==> Copying folder of include to build/$os"
+cp --recursive ./src/include ./build/$os/include
+echo "==> Copying folder of include to build/$os was successfully."
+echo "==> Copying folder of Scripts to build/$os"
+cp --recursive ./src/Scripts ./build/$os/Scripts
+echo "==> Copying folder of Scripts to build/$os was successfully."
+echo "==> Copying folder of utils to build/$os"
+cp --recursive ./src/utils ./build/$os/utils
+echo "==> Copying folder of utils to build/$os was successfully."
+echo "==> Copying folder of locale to build/$os"
+cp --recursive ./src/locale ./build/$os/locale
+echo "==> Copying folder of locale to build/$os was successfully."
+echo "==> Copying folder of Logo to build/$os"
+cp --recursive ./src/Logo ./build/$os/Icon
+echo "==> Copying folder of Logo to build/$os was successfully."
 #echo "=================================="
-
-echo "=================================="
