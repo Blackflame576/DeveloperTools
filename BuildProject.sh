fe3523273c27b5243d735e58179913569ca924d3
blackflame576 <pws15548@gmail.com>
2023-08-11 16:14:58 +0300

    Build for Linux

--- BuildProject.sh
+++ BuildProject.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "==> Building"
 unameOut=$(uname -a)
 case "${unameOut}" in
@@ -7,53 +8,29 @@ esac
 if [ $os == "Linux" ]
 then
   # Building
-	if [-d "./build/Linux"]
-	then
-	  g++ -o .\build\Linux\DeepForgeToolset.exe .\src\DeepForgeToolset.cpp -DCURL_STATICLIB -I ..\..\include -I .\src\include -L ..\..\lib\  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
-	else
-	  mkdir build
-    cd build
-    mkdir Linux
-    cd ..
-    g++ -o .\build\Linux\DeepForgeToolset.exe .\src\DeepForgeToolset.cpp -DCURL_STATICLIB -I ..\..\include -I .\src\include -L ..\..\lib\  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
-	fi
-	# Exists folder
-	if [! -d "./build/Linux/DB"]
-	then
-	  echo "==> Copying folder of DB to build/Linux ..."
-	  cp ./src/DB ./build/Linux/DB
-	  echo "==> Copying folder of DB to build/Windows was successfully."
-
-	if [! -d "./build/Linux/include"]
-	then
-	  echo "==> Copying folder of include to build/Linux"
-	  cp ./src/include ./build/Linux/include
-	  echo "==> Copying folder of include to build/Linux was successfully."
-
-	if [! -d "./build/Linux/Scripts"]
-	then
-	  echo "==> Copying folder of Scripts to build/Linux"
-	  cp ./src/Scripts ./build/Linux/Scripts
-	  echo "==> Copying folder of Scripts to build/Linux was successfully."
-
-	if [! -d "./build/Linux/utils"]
-	then
-	  echo "==> Copying folder of utils to build/Linux"
-	  cp ./src/utils ./build/Linux/utils
-	  echo "==> Copying folder of utils to build/Linux was successfully."
-
-	if [! -d "./build/Linux/locale"]
-	then
-	  echo "==> Copying folder of locale to build/Linux"
-	  cp ./src/locale ./build/Linux/locale
-	  echo "==> Copying folder of locale to build/Linux was successfully."
-
-	if [! -d "./build/Linux/Icon"]
-	then
-	  echo "==> Copying folder of Icon to build/Linux"
-	  cp ./src/Icon ./build/Linux/Icon
-	  echo "==> Copying folder of Icon to build/Linux was successfully."
-
+	mkdir build
+  cd build
+  mkdir Linux
+  cd ..
+  g++ -o ./build/Linux/DeepForgeToolset.exe ./src/DeepForgeToolset.cpp -DCURL_STATICLIB -I ../../include -I ./src/include -L ../../lib/  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
+	echo "==> Copying folder of DB to build/Linux ..."
+  cp --recursive ./src/DB ./build/Linux/DB
+  echo "==> Copying folder of DB to build/Windows was successfully."
+  echo "==> Copying folder of include to build/Linux"
+  cp --recursive ./src/include ./build/Linux/include
+  echo "==> Copying folder of include to build/Linux was successfully."
+  echo "==> Copying folder of Scripts to build/Linux"
+  cp --recursive ./src/Scripts ./build/Linux/Scripts
+  echo "==> Copying folder of Scripts to build/Linux was successfully."
+  echo "==> Copying folder of utils to build/Linux"
+  cp --recursive ./src/utils ./build/Linux/utils
+  echo "==> Copying folder of utils to build/Linux was successfully."
+  echo "==> Copying folder of locale to build/Linux"
+  cp --recursive ./src/locale ./build/Linux/locale
+  echo "==> Copying folder of locale to build/Linux was successfully."
+  echo "==> Copying folder of Logo to build/Linux"
+  cp --recursive ./src/Logo ./build/Linux/Icon
+  echo "==> Copying folder of Logo to build/Linux was successfully."
 elif [ $os == "macOS" ]
 then
 	cd build
