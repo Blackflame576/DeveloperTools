338f98cf2d1da58b1245c1790676ed15a90731b8
blackflame576 <pws15548@gmail.com>
2023-08-11 17:50:52 +0300

    Rewrite BuildProject.sh

--- BuildProject.sh
+++ BuildProject.sh
@@ -10,23 +10,23 @@ mkdir build
 cd build
 mkdir $os
 cd ..
-g++ -o ./build/$os/DeepForgeToolset ./src/DeepForgeToolset.cpp -DCURL_STATICLIB -I ../../include -I ./src/include -L ../../lib/  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++20
+g++ -o ./build/$os/DeepForgeToolset ./src/DeepForgeToolset.cpp -DCURL_STATICLIB -I ../../include -I ./src/include -L ../../lib/  -lcurl -Wreturn-type -ljsoncpp -lsqlite3 -std=c++2a
 echo "==> Copying folder of DB to build/$os ..."
-cp --recursive ./src/DB ./build/$os/DB
+cp -R ./src/DB ./build/$os/DB
 echo "==> Copying folder of DB to build/$os was successfully."
 echo "==> Copying folder of include to build/$os"
-cp --recursive ./src/include ./build/$os/include
+cp -R ./src/include ./build/$os/include
 echo "==> Copying folder of include to build/$os was successfully."
 echo "==> Copying folder of Scripts to build/$os"
-cp --recursive ./src/Scripts ./build/$os/Scripts
+cp -R ./src/Scripts ./build/$os/Scripts
 echo "==> Copying folder of Scripts to build/$os was successfully."
 echo "==> Copying folder of utils to build/$os"
-cp --recursive ./src/utils ./build/$os/utils
+cp -R ./src/utils ./build/$os/utils
 echo "==> Copying folder of utils to build/$os was successfully."
 echo "==> Copying folder of locale to build/$os"
-cp --recursive ./src/locale ./build/$os/locale
+cp -R ./src/locale ./build/$os/locale
 echo "==> Copying folder of locale to build/$os was successfully."
 echo "==> Copying folder of Logo to build/$os"
-cp --recursive ./src/Logo ./build/$os/Icon
+cp -R ./src/Logo ./build/$os/Icon
 echo "==> Copying folder of Logo to build/$os was successfully."
 #echo "=================================="
