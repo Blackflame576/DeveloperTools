99fd0d75bef13e4719ec6d0b06e93bc2bed3602a
blackflame576 <pws15548@gmail.com>
2023-09-01 14:01:11 +0300

    Refactoring BuildProject.sh

--- src/UpdateManager/include/UpdateManager_Windows.hpp
+++ src/UpdateManager/include/UpdateManager_Windows.hpp
@@ -1,7 +1,141 @@
+/*  The MIT License (MIT)
+    ============================================================================
+
+    ██████╗ ███████╗███████╗██████╗ ███████╗ ██████╗ ██████╗  ██████╗ ███████╗
+    ██╔══██╗██╔════╝██╔════╝██╔══██╗██╔════╝██╔═══██╗██╔══██╗██╔════╝ ██╔════╝
+    ██║  ██║█████╗  █████╗  ██████╔╝█████╗  ██║   ██║██████╔╝██║  ███╗█████╗
+    ██║  ██║██╔══╝  ██╔══╝  ██╔═══╝ ██╔══╝  ██║   ██║██╔══██╗██║   ██║██╔══╝
+    ██████╔╝███████╗███████╗██║     ██║     ╚██████╔╝██║  ██║╚██████╔╝███████╗
+    ╚═════╝ ╚══════╝╚══════╝╚═╝     ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+
+    ████████╗ ██████╗  ██████╗ ██╗     ███████╗███████╗████████╗
+    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝██╔════╝╚══██╔══╝
+       ██║   ██║   ██║██║   ██║██║     ███████╗█████╗     ██║
+       ██║   ██║   ██║██║   ██║██║     ╚════██║██╔══╝     ██║
+       ██║   ╚██████╔╝╚██████╔╝███████╗███████║███████╗   ██║
+       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝
+
+    ============================================================================
+    Copyright (c) 2023 DeepForge Technology
+    ============================================================================
+    Organization: DeepForge Technology
+    ============================================================================
+    Author: Blackflame576
+    ============================================================================
+    Created: 4 Juny 2023
+    ============================================================================
+*/
+#include <filesystem>
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#pragma comment(lib, "urlmon.lib")
+#include "Progressbar.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../DatabaseConnect.cpp"
+#include <map>
 
 using namespace std;
+using namespace DB;
+using namespace Bar;
 
-namespace Windows {
-    class Update
+namespace Windows
+{
+    int result;
+    int Percentage;
+    int TempPercentage = 0;
+    string Architecture;
+    float LastSize;
+    float LastTotalSize;
+    string Answer;
+    const string NewApplicationFolder = "C:\\ProgramData\\DeepForge\\DeepForge-Toolset";
+    const string NewTempFolder = NewApplicationFolder + "\\Temp";
+    ProgressBar_v1 progressbar;
+    const string DB_URL = "https://github.com/DeepForge-Technology/DeepForge-Toolset/releases/download/InstallerUtils/Versions.db";
+    std::filesystem::path ProjectDir = std::filesystem::current_path().generic_string();
+    string DB_PATH = NewTempFolder + "\\Versions.db";
+    string NameVersionTable = "WindowsVersions";
+
+    class WriteData : public IBindStatusCallback
+    {
+    public:
+        HRESULT __stdcall QueryInterface(const IID &, void **)
+        {
+            return E_NOINTERFACE;
+        }
+        ULONG STDMETHODCALLTYPE AddRef(void)
+        {
+            return 1;
+        }
+        ULONG STDMETHODCALLTYPE Release(void)
+        {
+            return 1;
+        }
+        HRESULT STDMETHODCALLTYPE OnStartBinding(DWORD dwReserved, IBinding *pib)
+        {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE GetPriority(LONG *pnPriority)
+        {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnLowResource(DWORD reserved)
+        {
+            return S_OK;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnStopBinding(HRESULT hresult, LPCWSTR szError)
+        {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE GetBindInfo(DWORD *grfBINDF, BINDINFO *pbindinfo)
+        {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnDataAvailable(DWORD grfBSCF, DWORD dwSize, FORMATETC *pformatetc, STGMEDIUM *pstgmed)
+        {
+            return E_NOTIMPL;
+        }
+        virtual HRESULT STDMETHODCALLTYPE OnObjectAvailable(REFIID riid, IUnknown *punk)
+        {
+            return E_NOTIMPL;
+        }
+
+        virtual HRESULT __stdcall OnProgress(ULONG ulProgress, ULONG ulProgressMax, ULONG ulStatusCode, LPCWSTR szStatusText)
+        {
+
+            Percentage = static_cast<float>(ulProgress) / static_cast<float>(ulProgressMax) * 100;
+            if (TempPercentage != Percentage && TempPercentage <= 98)
+            {
+                progressbar.Update(0.0, (float)(ulProgress), (float)(ulProgressMax));
+                LastSize = (float)(ulProgress);
+                LastTotalSize = (float)(ulProgressMax);
+                TempPercentage = Percentage;
+            }
+            return S_OK;
+        }
+    };
+    // Main class
+    class Updater
+    {
+    public:
+        Updater()
+        {
+            GetArchitectureOS();
+        }
+        void InstallLatestRelease();
+    private:
+        // Method for getting architecture of OS
+        void GetArchitectureOS()
+        {
+            #if defined(__x86_64__)
+                        Architecture = "amd64";
+            #elif __arm__
+                        Architecture = "arm64";
+            #endif
+        }
+    }
 }
