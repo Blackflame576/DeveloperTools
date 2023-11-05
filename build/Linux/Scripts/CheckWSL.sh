0c48ff1450b47e2537c425974409bd56da2fd8ba
Blackflame576 <88478228+Blackflame576@users.noreply.github.com>
2023-11-05 18:33:02 +0300

    Delete build directory

--- build/Linux/Scripts/CheckWSL.sh
+++
@@ -1,4 +0,0 @@
-if [[ $(grep -i Microsoft /proc/version) ]]; then
-    sudo apt-get update -y && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
-    sudo daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
-fi
